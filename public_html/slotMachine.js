e2d8c979b26a36c9f18866a31f33677a42c4c497
devin <devin@LAPTOP-TMETHJNR>
2021-12-14 18:52:46 -0600

    Slots addition

--- public_html/slotMachine.js
+++ public_html/slotMachine.js
@@ -1,7 +1,26 @@
 
-cash = 1000;
+var cash = 1000;
 
 while(cash > 0){
-    bet = Number(prompt('Enter your bet here:'));
-    
+   var bet = Number(prompt('Enter your bet here:'));
+    if(bet > cash){
+        bet = cash;
+    }else
+        prompt('You do not have enough money!');
 }
+
+var slots = ['0','1','2','3','4','5'];
+slots[0] = 'Cherries';
+slots[1] = 'Oranges';
+slots[2] = 'Plums';
+slots[3] = 'Bells';
+slots[4] = 'Melons';
+slots[5] = 'Bars';
+
+var chanceOne = Math.floor(Math.random()* slots.length);
+document.write (chanceOne + ' ');
+var chanceTwo = Math.floor(Math.random()* slots.length);
+document.write (chanceTwo + ' ');
+var chanceThree = Math.floor(Math.random()* slots.length);
+document.write (chanceThree + ' ');
+
